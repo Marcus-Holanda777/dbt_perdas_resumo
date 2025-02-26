172a350f066f6c02cf0468e91a0bb8ff4fa263d3
Marcus Holanda <mvsh777@hotmail.com>
2025-02-26 18:33:15 -0300

    feat(:main:) corrigido coluna referente a servicos no produto mestre

--- macros/servicos.sql
+++ macros/servicos.sql
@@ -1,4 +1,4 @@
-{%- macro servicos(alias="pm", column="capr_cd_categoria") -%}
+{%- macro servicos(alias="pm", column="capn_cd_categoria") -%}
  AND {{ alias }}.{{ column }} NOT LIKE '3%'
  AND {{ alias }}.{{ column }} NOT LIKE '1.101.009%'
  AND {{ alias }}.{{ column }} NOT LIKE '1.102.009%'
